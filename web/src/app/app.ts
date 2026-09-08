91253a2bb52335d65e14d1a183099f44bcb423cb
Mark van Kerkwyk <kerkwyk@gmail.com>
2026-09-09 09:58:27 +1000

    Change greeting from 'Hello, web' to 'Hello, World'

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
 })
 export class App {
-  protected readonly title = signal('web');
+  protected readonly title = signal('World');
 }
